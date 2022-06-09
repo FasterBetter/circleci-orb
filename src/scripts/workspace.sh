dc7cddfc86ed69e7a3e943bd271c9161a605e720
Jon Frisby <jfrisby@mrjoy.com>
2022-06-09 15:38:10 -0700

    Disable one more false positive.

--- src/scripts/workspace.sh
+++ src/scripts/workspace.sh
@@ -4,6 +4,8 @@ unset TF_WORKSPACE
 
 if [[ $workspace_parameter != "" ]]; then
   echo "[INFO] Provisioning workspace: $workspace"
+  # $module_path is defined by caller.
+  # shellcheck disable=SC2154
   ~/terraform/terraform -chdir="$module_path" workspace select -no-color "$workspace" || ~/terraform/terraform -chdir="$module_path" workspace new -no-color "$workspace"
 else
   echo "[INFO] Using default workspace"
