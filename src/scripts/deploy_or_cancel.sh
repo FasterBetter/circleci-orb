ac58a2c78e472ce6986a2a4a2fd9da2e739430fc
Jon Frisby <jfrisby@mrjoy.com>
2022-06-09 15:27:56 -0700

    https://www.shellcheck.net/wiki/SC2046

--- src/scripts/deploy_or_cancel.sh
+++ src/scripts/deploy_or_cancel.sh
@@ -44,9 +44,9 @@ GetRoleAndSfnArn() {
 AssumeRole() {
   echo "Assuming role ${ROLE_ARN}"
   if [ -z "$ROLE_EXTERNAL_ID" ]; then
-    eval $(aws sts assume-role --role-arn "${ROLE_ARN}" --role-session-name "${D_SERVICE_NAME}" | jq -r '.Credentials | "export AWS_ACCESS_KEY_ID=\(.AccessKeyId)\nexport AWS_SECRET_ACCESS_KEY=\(.SecretAccessKey)\nexport AWS_SESSION_TOKEN=\(.SessionToken)\n"')
+    eval "$(aws sts assume-role --role-arn "${ROLE_ARN}" --role-session-name "${D_SERVICE_NAME}" | jq -r '.Credentials | "export AWS_ACCESS_KEY_ID=\(.AccessKeyId)\nexport AWS_SECRET_ACCESS_KEY=\(.SecretAccessKey)\nexport AWS_SESSION_TOKEN=\(.SessionToken)\n"')"
   else
-    eval $(aws sts assume-role --external-id "${ROLE_EXTERNAL_ID}" --role-arn "${ROLE_ARN}" --role-session-name "${D_SERVICE_NAME}" | jq -r '.Credentials | "export AWS_ACCESS_KEY_ID=\(.AccessKeyId)\nexport AWS_SECRET_ACCESS_KEY=\(.SecretAccessKey)\nexport AWS_SESSION_TOKEN=\(.SessionToken)\n"')
+    eval "$(aws sts assume-role --external-id "${ROLE_EXTERNAL_ID}" --role-arn "${ROLE_ARN}" --role-session-name "${D_SERVICE_NAME}" | jq -r '.Credentials | "export AWS_ACCESS_KEY_ID=\(.AccessKeyId)\nexport AWS_SECRET_ACCESS_KEY=\(.SecretAccessKey)\nexport AWS_SESSION_TOKEN=\(.SessionToken)\n"')"
   fi
 }
 
